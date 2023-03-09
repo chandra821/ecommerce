f6d46d38be2111943d5ada2bcf2abfb426fb4a6c
chandra821 <73953659+chandra821@users.noreply.github.com>
2023-03-10 00:33:50 +0530

    updated

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import cors from "cors";
 import categoryRouts from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import {fileURLToPath} from "url";
 
 // configure env
 dotenv.config();
 
 // data base config
 connectDB();
+
+// esmodel fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 // rest object
 const app = express();
